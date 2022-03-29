a31f77d336a95ae291e64164bca2b25966161d45
Icaro-Salgado <icaro.salgado@mercadolivre.com>
2022-03-29 20:43:41 -0300

    test: finishing correct property value test

--- src/test/java/br/com/mercadolivre/defafioquality/service/PropertyServiceTests.java
+++ src/test/java/br/com/mercadolivre/defafioquality/service/PropertyServiceTests.java
@@ -1,5 +1,8 @@
 package br.com.mercadolivre.defafioquality.service;
 
+import br.com.mercadolivre.defafioquality.exceptions.DatabaseReadException;
+import br.com.mercadolivre.defafioquality.models.Property;
+import br.com.mercadolivre.defafioquality.models.Room;
 import br.com.mercadolivre.defafioquality.repository.PropertyRepository;
 import br.com.mercadolivre.defafioquality.services.PropertyService;
 import org.junit.jupiter.api.AfterEach;
@@ -8,7 +11,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -30,11 +35,21 @@ public class PropertyServiceTests {
     }
 
     @Test
-    public void testIfPropertyPriceIsCorrect() {
+    public void testIfPropertyPriceIsCorrect() throws DatabaseReadException {
+        // SETUP
         BigDecimal expected = BigDecimal.valueOf(32);
 
+        List<Room> fakeRooms = List.of(new Room("Room fake", 8.0, 4.0));
+
+        Property fakeProperty = new Property();
+        fakeProperty.setPropRooms(fakeRooms);
+
+        Mockito.when(this.propertyRepository.find(Mockito.any())).thenReturn(java.util.Optional.of(fakeProperty));
+
+        // ACT
         BigDecimal real = this.propertyService.calcPropertyPrice(UUID.randomUUID());
 
+        // ASSERT
         Assertions.assertEquals(expected, real);
     }
 }
