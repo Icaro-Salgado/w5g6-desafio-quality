3ddc83a4437d66465968170bee3426b928ccebf6
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-03-29 16:04:38 -0300

    feat: add initial blank Property service
    -
    
    close: #1

---
+++ src/main/java/br/com/mercadolivre/defafioquality/services/PropertyService.java
@@ -0,0 +1,16 @@
+package br.com.mercadolivre.defafioquality.services;
+
+import br.com.mercadolivre.defafioquality.models.Property;
+import br.com.mercadolivre.defafioquality.repository.ApplicationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PropertyService {
+
+    private final ApplicationRepository<Property, UUID> propertyRepository;
+
+}
