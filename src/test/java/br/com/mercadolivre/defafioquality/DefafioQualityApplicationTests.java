63887db60b628ba4fcbe1526a330a4f766a977bc
Icaro-Salgado <icaro.salgado@mercadolivre.com>
2022-03-29 14:40:41 -0300

    project structure

---
+++ src/test/java/br/com/mercadolivre/defafioquality/DefafioQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.defafioquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefafioQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
