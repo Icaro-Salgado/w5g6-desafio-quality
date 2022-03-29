c96d999250273993f23562458493eeae168d2210
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-03-29 15:40:33 -0300

    feat: add initial file manager struture
    -
    
    start: #1

---
+++ src/main/java/br/com/mercadolivre/defafioquality/exceptions/DatabaseManagementException.java
@@ -0,0 +1,9 @@
+package br.com.mercadolivre.defafioquality.exceptions;
+
+public class DatabaseManagementException extends Exception {
+
+    public DatabaseManagementException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ src/main/java/br/com/mercadolivre/defafioquality/exceptions/DbEntryAlreadyExists.java
@@ -0,0 +1,9 @@
+package br.com.mercadolivre.defafioquality.exceptions;
+
+public class DbEntryAlreadyExists extends Exception {
+
+    public DbEntryAlreadyExists(String msg) {
+        super(msg);
+    }
+
+}
