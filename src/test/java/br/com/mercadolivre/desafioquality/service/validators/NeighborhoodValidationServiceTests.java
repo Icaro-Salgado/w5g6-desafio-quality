a3f1af3167f33c74c402213a2e8b9be5e5fcf111
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-03-30 17:46:42 -0300

    test: adding tests for validation service

--- src/test/java/br/com/mercadolivre/desafioquality/service/validators/NeighborhoodValidationServiceTests.java
+++ src/test/java/br/com/mercadolivre/desafioquality/service/validators/NeighborhoodValidationServiceTests.java
@@ -1,5 +1,46 @@
 package br.com.mercadolivre.desafioquality.service.validators;
 
-public class NeighborhoodValidationServiceTests {
+import br.com.mercadolivre.desafioquality.exceptions.DatabaseManagementException;
+import br.com.mercadolivre.desafioquality.exceptions.DatabaseReadException;
+import br.com.mercadolivre.desafioquality.models.Neighborhood;
+import br.com.mercadolivre.desafioquality.repository.NeighborhoodRepository;
+import br.com.mercadolivre.desafioquality.services.validators.NeighborhoodValidationService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class NeighborhoodValidationServiceTests {
+    private NeighborhoodValidationService neighborhoodValidationService;
+    private NeighborhoodRepository neighborhoodRepository;
+
+    @BeforeEach
+    public void setUp() {
+        this.neighborhoodRepository = Mockito.mock(NeighborhoodRepository.class);
+
+        this.neighborhoodValidationService = new NeighborhoodValidationService(neighborhoodRepository);
+    }
+
+    @AfterEach
+    public void reset() {
+        this.neighborhoodValidationService = null;
+    }
+
+    @Test
+    public void testIfPassesForAllValidations() throws DatabaseReadException, DatabaseManagementException {
+        // SETUP
+        List<Neighborhood> fakeNeighborhoods = new ArrayList<>();
+        fakeNeighborhoods.add(Neighborhood.builder().nameDistrict("Não é fake").build());
+
+        Mockito.when(this.neighborhoodRepository.read()).thenReturn(fakeNeighborhoods);
+
+        // ACT
+        this.neighborhoodValidationService.validate("Não é fake");
+
+        // ASSERT
+        Mockito.verify(this.neighborhoodRepository, Mockito.times(1)).read();
+    }
 }
