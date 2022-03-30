96e5a84b9c036269fcb5a9ed6915d831de590de1
Icaro-Salgado <icaro.salgado@mercadolivre.com>
2022-03-29 21:03:19 -0300

    feat: adding response validation
    continue #17

--- src/main/java/br/com/mercadolivre/defafioquality/services/PropertyService.java
+++ src/main/java/br/com/mercadolivre/defafioquality/services/PropertyService.java
@@ -1,12 +1,14 @@
 package br.com.mercadolivre.defafioquality.services;
 
 import br.com.mercadolivre.defafioquality.exceptions.NullIdException;
+import br.com.mercadolivre.defafioquality.exceptions.PropertyNotFoundException;
 import br.com.mercadolivre.defafioquality.models.Property;
 import br.com.mercadolivre.defafioquality.repository.ApplicationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -18,6 +20,12 @@ public class PropertyService {
     public BigDecimal calcPropertyPrice(UUID propertyId) throws NullIdException {
         if(propertyId == null) {
             throw new NullIdException("O id é nulo!");
+        }
+
+        Optional<Property> response = propertyRepository.find(propertyId);
+
+        if(response.isEmpty()) {
+            throw new PropertyNotFoundException("Propriedade não encontrada");
         }
 
         return null;
