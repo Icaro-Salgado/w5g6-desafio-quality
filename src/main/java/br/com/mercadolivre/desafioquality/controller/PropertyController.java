379cb5b709135f3d8f9b6b0f3ef3b062d58f529d
pedroLSoares <pedro.levada@mercadolivre.com>
2022-03-30 16:27:04 -0300

    feat: Add swagger
    open: #15

--- src/main/java/br/com/mercadolivre/desafioquality/controller/PropertyController.java
+++ src/main/java/br/com/mercadolivre/desafioquality/controller/PropertyController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/property")
+@RequestMapping("/api/v1")
 @AllArgsConstructor
 public class PropertyController {
 
