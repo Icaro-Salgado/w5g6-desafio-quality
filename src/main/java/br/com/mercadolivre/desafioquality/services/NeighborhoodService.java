7a9e2a09a93531a97895504904a8fcf23de5cfcc
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-03-31 14:46:36 -0300

    feat: add find and delete neighborhood operations
    -
    
    start #45

--- src/main/java/br/com/mercadolivre/desafioquality/services/NeighborhoodService.java
+++ src/main/java/br/com/mercadolivre/desafioquality/services/NeighborhoodService.java
@@ -1,12 +1,34 @@
 package br.com.mercadolivre.desafioquality.services;
 
+import br.com.mercadolivre.desafioquality.exceptions.DatabaseReadException;
+import br.com.mercadolivre.desafioquality.exceptions.DatabaseWriteException;
+import br.com.mercadolivre.desafioquality.exceptions.NeighborhoodNotFoundException;
+import br.com.mercadolivre.desafioquality.models.Neighborhood;
+import br.com.mercadolivre.desafioquality.repository.NeighborhoodRepository;
+import lombok.AllArgsConstructor;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@AllArgsConstructor
 public class NeighborhoodService {
+
+    private NeighborhoodRepository neighborhoodRepository;
 
     public void createNeighborhood(){}
 
     public void listNeighborhood(){}
 
-    public void getNeighborhoodById(){}
+    public Neighborhood getNeighborhoodById(UUID id) throws DatabaseReadException {
+        Optional<Neighborhood> neighborhoodExists = this.neighborhoodRepository.find(id);
+        if (neighborhoodExists.isPresent()) {
+            return neighborhoodExists.get();
+        }
+        throw new NeighborhoodNotFoundException("Bairro não encontrado");
+    }
 
-    public void deleteNeighborhoodById(){}
+    public void deleteNeighborhoodById(UUID id) throws DatabaseReadException, DatabaseWriteException {
+        Neighborhood neighborhood = getNeighborhoodById(id);
+        this.neighborhoodRepository.delete(neighborhood);
+    }
 }
