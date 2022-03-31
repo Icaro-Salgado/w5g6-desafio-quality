2e6ec065f7bd2d0ec82c13c6ad85f79e9967012a
esutil <evandro.sutil@mercadolivre.com>
2022-03-31 14:55:54 -0300

    fix: change neighborhood validation
    -
    
    starts #42

--- src/main/java/br/com/mercadolivre/desafioquality/dto/NeighborhoodDTO.java
+++ src/main/java/br/com/mercadolivre/desafioquality/dto/NeighborhoodDTO.java
@@ -1,21 +1,35 @@
 package br.com.mercadolivre.desafioquality.dto;
 
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import br.com.mercadolivre.desafioquality.models.Neighborhood;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@AllArgsConstructor
+@Builder
+@Data
 public class NeighborhoodDTO {
 
     private UUID id;
 
-    @NotEmpty(message = "O bairro não pode ficar vazio! \n")
-    @Size(max = 45,message = "O comprimento do bairro não pode exceder 45 caracteres!\n")
+    @NotEmpty(message = "O bairro não pode ficar vazio!")
+    @Size(max=45, message = "O comprimento do bairro não pode exceder 45 caracteres!")
     private String nameDistrict;
 
-    @NotEmpty(message = "O valor do metro quadrado do bairro não pode ficar vazio!\n")
-    @Digits(integer = 13,fraction = 2,message = "O valor do metro quadrado não pode exceder 13 digitos!\n")
+    @NotNull(message = "O valor do metro quadrado do bairro não pode ficar vazio!")
+    @DecimalMin(value="0.0", inclusive = false, message = "O valor do metro quadrado do bairro não ser menor ou igual a zero!")
+    @Digits(integer = 13,fraction = 2,message = "O valor do metro quadrado não pode exceder 13 digitos!")
     private BigDecimal valueDistrictM2;
 
+    public Neighborhood toModel() {
+
+        return Neighborhood.builder()
+                .nameDistrict(this.nameDistrict)
+                .valueDistrictM2(this.valueDistrictM2)
+                .build();
+    }
 }
