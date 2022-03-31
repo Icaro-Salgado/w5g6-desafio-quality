48a34e102926ce7c0f00cc5f451f84ad3e00adc2
pedroLSoares <pedro.levada@mercadolivre.com>
2022-03-31 11:48:53 -0300

    feat: Crete default files for neighborhood endpoints creation

---
+++ src/main/java/br/com/mercadolivre/desafioquality/services/NeighborhoodService.java
@@ -0,0 +1,12 @@
+package br.com.mercadolivre.desafioquality.services;
+
+public class NeighborhoodService {
+
+    public void createNeighborhood(){}
+
+    public void listNeighborhood(){}
+
+    public void getNeighborhoodById(){}
+
+    public void deleteNeighborhoodById(){}
+}
