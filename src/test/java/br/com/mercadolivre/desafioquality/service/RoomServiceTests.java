43003454541232c5fb09c7548573d00e048324c3
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-03-30 22:18:22 -0300

    chore: add some descriptions for the tests
    -
    
    continue #38

--- src/test/java/br/com/mercadolivre/desafioquality/service/RoomServiceTests.java
+++ src/test/java/br/com/mercadolivre/desafioquality/service/RoomServiceTests.java
@@ -2,6 +2,7 @@ package br.com.mercadolivre.desafioquality.service;
 
 import br.com.mercadolivre.desafioquality.models.Room;
 import br.com.mercadolivre.desafioquality.services.RoomService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -17,51 +18,53 @@ public class RoomServiceTests {
     final private RoomService roomService = new RoomService();
 
     @Test
+    @DisplayName("Given a valid room, when call calcArea, then return width multiplied by length")
     public void testIfTotalAreaPerRoomIsCorrect() {
-
         expected = 3.5 * 4.5;
-
         Room quartoEva = new Room("Quarto do Eva", 3.50, 4.5);
+
         Double areaQuartoEva = roomService.calcArea(quartoEva);
 
         assertEquals(expected, areaQuartoEva);
     }
 
     @Test
+    @DisplayName("Given a room containing negative width, when call calcArea, then return 0.0")
     public void testIfTotalAreaAnyRoomHasNegativeValue() {
         expected = 0.0;
-
         Room anyRoom = new Room("Quarto do Gasparzinho", -10.0, 8.40);
+
         Double areaTotal = roomService.calcArea(anyRoom);
 
         assertEquals(expected, areaTotal);
     }
 
     @Test
+    @DisplayName("Given a room containing null length, when call calcArea, then return 0.0")
     public void testIfTotalAreaAnyRoomHasNullValue() {
         expected = 0.0;
-
         Room anyRoom = new Room("Quarto do Gasparzinho", 6.20, null);
+
         Double areaTotal = roomService.calcArea(anyRoom);
 
         assertEquals(expected, areaTotal);
     }
 
     @Test
+    @DisplayName("Given a non existing room, when call calcArea, then return 0.0")
     public void testIfTotalAreaAnyRoomIsNull() {
         expected = 0.0;
-
         Room nonExistingRoom = null;
+
         Double areaTotal = roomService.calcArea(nonExistingRoom);
 
         assertEquals(expected, areaTotal);
     }
 
     @Test
+    @DisplayName("Given a list of valid rooms, when call calcArea, then return the sum of all room area")
     public void testIfTotalAreaAllRoomsIsCorret() {
-
-        expected = ((12.0 * 10.0) + (6.50 * 8.40) + (3.50*4.50));
-
+        expected = ((12.0 * 10.0) + (6.50 * 8.40) + (3.50 * 4.50));
         List<Room> rooms = List.of(
                 new Room("Quarto Pedro", 12.0, 10.0),
                 new Room("Quarto Klin", 6.50, 8.40),
@@ -73,9 +76,9 @@ public class RoomServiceTests {
     }
 
     @Test
+    @DisplayName("Given a list of rooms where one of them contains 0.0 width, when call calcArea, then return 0.0")
     public void testIfTotalAreaAnyRoomHasZeroAsWidth() {
         expected = 0.0;
-
         List<Room> rooms = List.of(
                 new Room("Quarto Pedro", 12.0, 10.0),
                 new Room("Quarto Klin", 0.0, 8.40),
@@ -84,15 +87,6 @@ public class RoomServiceTests {
         Double areaTotal = roomService.calcArea(rooms);
 
         assertEquals(expected, areaTotal);
-    }
-
-    @Test
-    public void testIfReturnErrorWhenReceiveNullValue(){
-        expected = 0.0;
-        Double roomArea = roomService.calcArea((Room) null);
-
-        assertEquals(expected, roomArea);
-
     }
 
 }
