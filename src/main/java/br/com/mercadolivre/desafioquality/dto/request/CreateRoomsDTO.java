518cd19ae019b259971275f11be9d765b2a4e89c
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-03-31 13:19:20 -0300

    fix: minor fixes on DTOs name
    continue #43

--- src/main/java/br/com/mercadolivre/desafioquality/dto/request/CreateRoomsDTO.java
+++ src/main/java/br/com/mercadolivre/desafioquality/dto/request/CreateRoomsDTO.java
@@ -1,23 +1,24 @@
 package br.com.mercadolivre.desafioquality.dto.request;
 
 import br.com.mercadolivre.desafioquality.models.Room;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
+@Data
+@AllArgsConstructor
 public class CreateRoomsDTO {
     @NotEmpty(message = "O campo não pode estar vazio.\n")
     @Size(max = 30,message = "O comprimento do cômodo não pode exceder 30 caracteres.")
     @Pattern(regexp = "^\\b[A-Z].*\\b", message = "O nome do comodo deve começar com uma letra maiúscula!")
     String name;
 
-    @NotEmpty(message = "A largura do cômodo não pode estar vazia. \n")
+    @NotNull(message = "A largura do cômodo não pode estar vazia. \n")
     @DecimalMax(value = "25",message = "A largura máxima permitida por cômodo é de 25 metros.")
     Double width;
 
-    @NotEmpty(message = "O comprimento do cômodo não pode estar vazio. \n")
+    @NotNull(message = "O comprimento do cômodo não pode estar vazio. \n")
     @DecimalMax(value = "33",message = "O comprimento máximo permitido por cômodo é de 33 metros.")
     Double length;
 
