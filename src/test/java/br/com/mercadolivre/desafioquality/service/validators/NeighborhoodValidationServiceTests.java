e9aa7ce3e5960d6d7cfd424d3197d6f9e3f99095
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-03-30 22:10:23 -0300

    hotfix: adjust validate neighborhood to return an boolean
    Change the return of validate from void to boolean, which makes it
    easier to know if the neighborhood is valid or not
    
    start #38

--- src/test/java/br/com/mercadolivre/desafioquality/service/validators/NeighborhoodValidationServiceTests.java
+++ src/test/java/br/com/mercadolivre/desafioquality/service/validators/NeighborhoodValidationServiceTests.java
@@ -2,16 +2,17 @@ package br.com.mercadolivre.desafioquality.service.validators;
 
 import br.com.mercadolivre.desafioquality.exceptions.DatabaseManagementException;
 import br.com.mercadolivre.desafioquality.exceptions.DatabaseReadException;
+import br.com.mercadolivre.desafioquality.exceptions.NeighborhoodNotFoundException;
+import br.com.mercadolivre.desafioquality.exceptions.PropertyNotFoundException;
 import br.com.mercadolivre.desafioquality.models.Neighborhood;
 import br.com.mercadolivre.desafioquality.repository.NeighborhoodRepository;
 import br.com.mercadolivre.desafioquality.services.validators.NeighborhoodValidationService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class NeighborhoodValidationServiceTests {
     private NeighborhoodValidationService neighborhoodValidationService;
@@ -30,17 +31,33 @@ public class NeighborhoodValidationServiceTests {
     }
 
     @Test
-    public void testIfPassesForAllValidations() throws DatabaseReadException, DatabaseManagementException {
+    @DisplayName("Given a String containing an existing Neighborhood, when call validate method, then return true")
+    public void testIfNeighborhoodIsValid() throws DatabaseReadException, DatabaseManagementException {
         // SETUP
         List<Neighborhood> fakeNeighborhoods = new ArrayList<>();
         fakeNeighborhoods.add(Neighborhood.builder().nameDistrict("Não é fake").build());
-
         Mockito.when(this.neighborhoodRepository.read()).thenReturn(fakeNeighborhoods);
 
         // ACT
-        this.neighborhoodValidationService.validate("Não é fake");
+        boolean neighborhoodExists = this.neighborhoodValidationService.validate("Não é fake");
 
         // ASSERT
-        Mockito.verify(this.neighborhoodRepository, Mockito.times(1)).read();
+        Assertions.assertTrue(neighborhoodExists);
+    }
+
+    @Test
+    @DisplayName("Given a String of non existing Neighborhood, when call validate method, then throw an error containing \"Bairro não encontrado\"")
+    public void testIfNeighborhoodIsNotValid() throws DatabaseReadException, DatabaseManagementException {
+        // SETUP
+        List<Neighborhood> fakeNeighborhoods = new ArrayList<>();
+        Mockito.when(this.neighborhoodRepository.read()).thenReturn(fakeNeighborhoods);
+
+        // ACT
+        Exception thrown = Assertions.assertThrows(
+                NeighborhoodNotFoundException.class, () -> this.neighborhoodValidationService.validate("É fake")
+        );
+
+        // ASSERT
+        Assertions.assertEquals("Bairro não encontrado", thrown.getMessage());
     }
 }
