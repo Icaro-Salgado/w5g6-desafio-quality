27a9acd74860fdb4f7edb938486c558f8bf4979d
Evandro de Nadai Sutil <evandrosutil@gmail.com>
2022-04-01 05:45:37 -0700

    improvement: changes return of createneighborhood (#61)
    * improvement: changes return of `createNeighborhood` to UUID
    
    * improvement: tests if the correct UUID is returned

--- src/main/java/br/com/mercadolivre/desafioquality/services/NeighborhoodService.java
+++ src/main/java/br/com/mercadolivre/desafioquality/services/NeighborhoodService.java
@@ -19,7 +19,7 @@ public class NeighborhoodService {
 
     private NeighborhoodRepository neighborhoodRepository;
 
-    public Neighborhood createNeighborhood(Neighborhood newNeighborhood) throws DbEntryAlreadyExists, DatabaseWriteException, DatabaseReadException {
+    public UUID createNeighborhood(Neighborhood newNeighborhood) throws DbEntryAlreadyExists, DatabaseWriteException, DatabaseReadException {
 
         List<Neighborhood> neighborhoods = neighborhoodRepository.read();
 
@@ -36,7 +36,9 @@ public class NeighborhoodService {
         }
 
         newNeighborhood.setId(UUID.randomUUID());
-        return neighborhoodRepository.add(newNeighborhood);
+        neighborhoodRepository.add(newNeighborhood);
+
+        return newNeighborhood.getId();
     }
 
     public void listNeighborhood(){}
