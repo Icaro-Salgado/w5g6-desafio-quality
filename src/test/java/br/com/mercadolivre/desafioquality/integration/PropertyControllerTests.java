d0456257eac22849aded28916d8f74bb0ae82ecb
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-04-01 13:16:04 -0300

    test: writing tests for get routes

--- src/test/java/br/com/mercadolivre/desafioquality/integration/PropertyControllerTests.java
+++ src/test/java/br/com/mercadolivre/desafioquality/integration/PropertyControllerTests.java
@@ -4,18 +4,24 @@ import br.com.mercadolivre.desafioquality.models.Neighborhood;
 import br.com.mercadolivre.desafioquality.models.Property;
 import br.com.mercadolivre.desafioquality.models.Room;
 import br.com.mercadolivre.desafioquality.test_utils.DatabaseUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import netscape.javascript.JSObject;
 import org.junit.jupiter.api.*;
+import org.mockito.internal.matchers.Any;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -51,6 +57,7 @@ public class PropertyControllerTests {
         databaseUtils.deleteDatabase();
     }
 
+    // TODO: Extrair método para a propety test utils, mas depois que fizer todos os testes de integração
     private List<Property> populateFakeDatabase() throws IOException {
         List<Property> properties = new ArrayList<>();
 
@@ -72,6 +79,24 @@ public class PropertyControllerTests {
                 .propName("Casa sem nome")
                 .propDistrict("Bairro legal")
                 .propRooms(rooms)
+                .propArea(68.0)
+                .propValue(BigDecimal.valueOf(200.0).multiply(BigDecimal.valueOf(68)))
+                .build();
+        properties.add(property);
+
+        rooms = List.of(
+                Room.builder().roomName("Ola").roomWidth(2.0).roomLength(34.0).build(),
+                Room.builder().roomName("Ola 2").roomWidth(2.0).roomLength(34.0).build()
+        );
+
+        property = Property
+                .builder()
+                .id(UUID.fromString("992b27ac-3f12-42e2-9925-b2f922c48ded"))
+                .propName("Casa com nome")
+                .propDistrict("Bairro legal")
+                .propArea(68.0 * 2)
+                .propValue(BigDecimal.valueOf(200.0).multiply(BigDecimal.valueOf(68 * 2)))
+                .propRooms(rooms)
                 .build();
         properties.add(property);
 
@@ -92,33 +117,38 @@ public class PropertyControllerTests {
     }
 
     @Test
-    @DisplayName("PropertyController - GET - /api/v1/property")
+    @DisplayName("PropertyController - GET - /api/v1/property/")
     public void testListProperties() throws Exception {
         List<Property> fakeProperties = populateFakeDatabase();
 
         // Building response
-        String response = "[";
-
+        ArrayList<HashMap> mapResponse = new ArrayList<>();
         for (Property property : fakeProperties) {
+            HashMap mappedObject = new ObjectMapper().convertValue(property, HashMap.class);
 
-            response = response.concat("{");
-            response = response.concat("\"id\":").concat(property.getId().toString()).concat(",\n");
-            response = response.concat("\"propName\":").concat(property.getPropName()).concat(",\n");
+            // Removing what is not used on list view
+            mappedObject.remove("largestRoomId");
+            mappedObject.remove("propRooms");
+            mappedObject.remove("propArea");
 
-            if (fakeProperties.iterator().next() != null) {
-                response = response.concat("},\n");
-            } else {
-                response = response.concat("}\n");
-            }
+            // Changing some key values
+            Object obj = mappedObject.remove("propValue");
+            mappedObject.put("price", obj);
 
+            // Adding extra returns
+            mappedObject.put("uri", "http://localhost/api/v1/property/".concat(property.getId().toString()));
+            mappedObject.put("roomsQty", property.getPropRooms().size());
+
+            mapResponse.add(mappedObject);
         }
-        response = response.concat("]");
+        String response = new ObjectMapper().writeValueAsString(mapResponse);
 
-        System.out.println(response);
-
-        mockMvc.perform(MockMvcRequestBuilders.
-                get("/api/v1/property/property-area/{propertyId}", "77b3737d-7450-4d94-8f95-936e2c17e2cc"))
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.
+                get("/api/v1/property/"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.totalArea").value(68.0));
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().json(response)).andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
     }
 }
